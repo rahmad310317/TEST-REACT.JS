99c861eeb6fa16e9c764341aef231fcf94b7e890
DESKTOP-4QMMAJQ\User <setyaamanbudi@gmail.com>
2024-06-08 14:16:50 +0700

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
 export default function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+    <div className="gap-2 flex justify-center bg-blue-600 min-h-screen items-center">
         <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
           Buy now
+        </button>
+        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+          Add to bag
         </button>
     </div>
   )
